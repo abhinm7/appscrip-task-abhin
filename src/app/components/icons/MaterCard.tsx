c3f5c9829fe9abc819a82cdc06b067c65fd77357
ABHIN M <abhin18m@gmail.com>
2026-03-30 14:55:23 +0530

    change icon size properties

--- src/app/components/icons/MaterCard.tsx
+++ src/app/components/icons/MaterCard.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MaterCard = () => {
     return (
-        <svg width="56" height="35" viewBox="0 0 56 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg width="50" height="30" viewBox="0 0 56 35" fill="none" xmlns="http://www.w3.org/2000/svg">
             <rect x="0.5" y="0.5" width="55" height="34" rx="4.5" fill="white" stroke="#FFF2F5" />
             <path d="M22.026 27.052C27.5633 27.052 32.0521 22.5632 32.0521 17.026C32.0521 11.4888 27.5633 7 22.026 7C16.4888 7 12 11.4888 12 17.026C12 22.5632 16.4888 27.052 22.026 27.052Z" fill="#EB001B" />
             <path d="M33.4845 27.052C39.0218 27.052 43.5106 22.5632 43.5106 17.026C43.5106 11.4888 39.0218 7 33.4845 7C27.9473 7 23.4585 11.4888 23.4585 17.026C23.4585 22.5632 27.9473 27.052 33.4845 27.052Z" fill="#F79E1B" />
